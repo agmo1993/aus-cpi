dfeef5ff9ab6dd6663d1629646e587a55256ea54
Abdul Rehman Mohammad <mohammad.abdul@abc.net.au>
2023-04-22 09:43:07 +1000

    updated inserts scripts, split monthly into seperate script

--- backend/insert_monthly.py
+++ backend/insert_monthly.py
@@ -14,39 +14,35 @@ def main():
     logging.info("Starting program to insert cpi data in table")
     logging.info("Reading data file aus_cpi_timeseries.csv")
 
-    # read the first row of the CSV file
-    header = pd.read_csv('./data/aus_cpi_timeseries.csv', nrows=1).columns
-    # Read the CSV file into a pandas DataFrame
-    df = pd.read_csv('./data/aus_cpi_timeseries.csv', skiprows=50010, names=header)
-
     # Connect to a PostgreSQL database
     conn = psycopg2.connect(
-        host=os.environ['DB_HOST'],
+        host=os.getenv('DB_HOST'),
         port=5432,
         database="auscpidb",
-        user= os.environ['DB_USER'],
-        password=os.environ['DB_PASS']
+        user= os.getenv('DB_USER'),
+        password=os.getenv('DB_PASS')
     )
 
-    logging.info("Inserting rows into table cpi_index")
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv('./data/aus_cpi_timeseries_monthly.csv')
+
+    logging.info("Inserting rows into table cpi_index_monthly")
     # Insert the rows into the table
     for index, row in df.iterrows():
         try:
             cur = conn.cursor()
             cur.execute('''
-                INSERT INTO auscpi.cpi_index (publish_date, seriesid, cpi_value, item, city)
+                INSERT INTO auscpi.cpi_index_monthly (publish_date, seriesid, cpi_value, item, city)
                 VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;
             ''', (row['Date'], row['Series ID'], row['CPI Value'], row['Item'], row['Location']))
-            conn.commit()
             logging.info(f"Inserted {row['Series ID']} for {row['Date']}")
+            conn.commit()
         except Exception as e:
             conn.rollback()
             logging.error(e)
 
     logging.info("Refreshing Materialized Views")
     # refresh materialized views
-    cur.execute("REFRESH MATERIALIZED VIEW auscpi.cpi_pct_quarterly;")
-    cur.execute("REFRESH MATERIALIZED VIEW auscpi.cpi_pct_yearly;")
     cur.execute("REFRESH MATERIALIZED VIEW auscpi.cpi_pct_monthly;")
     cur.execute("REFRESH MATERIALIZED VIEW auscpi.cpi_pct_yearly_base2017;")
 
